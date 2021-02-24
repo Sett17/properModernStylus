cab0f932fd84443077d3d9ed181f7c9cfb17dd12
Sett17 <raik.rohde@gmail.com>
2021-02-24 21:20:06 +0100

    layout @02/24/21 21:20:01

--- android/app/src/main/java/m/sett/propermodernstylus/MainActivity.kt
+++ android/app/src/main/java/m/sett/propermodernstylus/MainActivity.kt
@@ -1,11 +1,16 @@
 package m.sett.propermodernstylus
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
 }
