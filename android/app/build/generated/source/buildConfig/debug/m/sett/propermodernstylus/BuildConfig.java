012dc10e4efdec931c24a9268b0776083f2879bc
Sett17 <raik.rohde@gmail.com>
2021-03-11 17:04:05 +0100

    build with ak included @03/11/21 17:03:41

---
+++ android/app/build/generated/source/buildConfig/debug/m/sett/propermodernstylus/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package m.sett.propermodernstylus;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "m.sett.propermodernstylus";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
